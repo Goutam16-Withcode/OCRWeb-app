baa1c4b4024680dd68e1a963f88e6a66ac588ca9
Goutam16-Withcode <goutamshaarma001212@gmail.com>
2024-09-28 19:20:43 +0000

    updateapp

--- ocr.py
+++ ocr.py
@@ -4,12 +4,12 @@ import nltk
 from nltk.corpus import stopwords
 from collections import Counter
 
-image_path = '/content/c.png'
-img = Image.open('/content/c.png')
+##image_path = '/content/c.png'
+##img = Image.open('/content/c.png')
 
 
-text = pytesseract.image_to_string(img)
-print("Extracted Text:\n", text)
+extracted_text = pytesseract.image_to_string(img)
+print("Extracted Text:\n", extracted_text)
 
 
 nltk.download('stopwords')
@@ -17,11 +17,11 @@ nltk.download('stopwords')
 
 def extract_keywords(text):
     stop_words = set(stopwords.words('english'))
-    words = text.split()
+    words =extracted_text.split()
     keywords = [word for word in words if word.lower() not in stop_words]
     keyword_freq = Counter(keywords)
     return keyword_freq.most_common(10)
 
-keywords = extract_keywords(text)
+keywords = extract_keywords(extracted_text)
 print("Top Keywords:\n", keywords)
 img.show()
